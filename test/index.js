4cb2c561506a69682b8eb4bf573d6e137f57293c
Jon Sherrard <jon.sherrard@shortlist.com>
2018-12-11 01:07:37 +0000

    put test bits in a folder

--- test/index.js
+++ test/index.js
@@ -1,8 +1,10 @@
 const util = require("util");
 const fs = require("fs");
-const mdxToMdxAst = require("./index");
+const path = require("path");
+const mdxToMdxAst = require("../index");
 
-const mdxString = fs.readFileSync("./fixture.mdx", "utf-8");
+const fixturePath = path.resolve(__dirname, "fixture.mdx");
+const mdxString = fs.readFileSync(fixturePath, "utf-8");
 
 const test = async function() {
   const mdxAst = await mdxToMdxAst(mdxString);
